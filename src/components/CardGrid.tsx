f5fe917c2bd11a4a532738d96f29b11ddf08f213
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2025-03-06 12:42:44 -0300

    Cambio en la paleta de colores

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 const CardGrid = ({ cards } : Props) =>{
     return(
-        <div className="min-h-screen bg-Falu-red py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen bg-Gray0 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto flex items-center flex-col gap-10">
-                <h1 className="title"> Featured Emogis</h1>
+                <h1 className="text-white font-bold text-4xl"> EMOGIS</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {cards.map((card) => (
                         <CardItem key={card.id} {...card}/> 
